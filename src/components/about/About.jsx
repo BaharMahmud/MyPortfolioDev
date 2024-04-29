ee47f268b11b494582ed689aa110d8cfcbb8f730
BaharMahmud <bahar.a.mahmud@gmail.com>
2024-04-28 21:49:48 -0400

    changes ti image pathes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import myPhoto from '../../assets/myPhoto.png';
 import './about.css';
 const About = () => {
   // Scroll the About component into view when the component mounts
@@ -7,7 +8,7 @@ const About = () => {
   }, []);
   return (
     <div className="about" id="about">
-      <img src='/src/assets/myPhoto.jpg' alt="Profile" className="profile-pic" />
+      <img src={myPhoto} alt="Profile" className="profile-pic" />
 
       <h3>About Me</h3>
     <p>As a full stack developer, my journey has been driven by an unyielding passion for creating seamless user experiences and robust backend solutions. With a keen eye for design and a methodical approach to problem-solving, I've honed my skills in both front-end and back-end technologies, enabling me to craft intuitive interfaces and scalable server architectures. My portfolio showcases a spectrum of projects that reflect a deep understanding of the full software development lifecycle, from conceptualization to deployment. Each project is a testament to my ability to work with a diverse stack, including but not limited to React, Node.js, and database management systems, and highlights my commitment to writing clean, maintainable code. I take pride in my adaptability and my collaborative spirit, thriving in environments that challenge me to learn and grow alongside my peers. Whether I'm optimizing API performance or ensuring responsive design, my ultimate goal remains the same: to deliver exceptional digital solutions that drive success.
--- src/components/myprojects/MyProjects.jsx
+++ src/components/myprojects/MyProjects.jsx
@@ -1,46 +1,53 @@
 import React from 'react';
 import './myprojects.css';
+import generatePassword from '../../assets/generatePassword.png'
+import findYourDreamJob from '../../assets/findYourDreamJob.png'
+import greenHouse from '../../assets/greenHouse.png'
+import checktheWeather from '../../assets/checktheWeather.png'
+import noteTaker from '../../assets/noteTaker.png'
+import textEditor from '../../assets/textEditor.png'
+
 // Dummy data for project details
 const projects = [
   {
     id: 1,
     name: 'Generate a random Password',
-    imageUrl: 'src/assets/generatePassword.png', // Replace with your image path
+    imageUrl: generatePassword, // Replace with your image path
     githubUrl: 'https://github.com/BaharMahmud/GeneratePassword',
     deployedUrl: 'hhttps://baharmahmud.github.io/GeneratePassword/',
   },
   {
     id: 2,
     name: 'GrrenHose Plant Project',
-    imageUrl: 'src/assets/greenHouse.png', // Replace with your image path
+    imageUrl: greenHouse, // Replace with your image path
     githubUrl: 'https://github.com/BaharMahmud/PlantData',
     deployedUrl: 'https://plant-data-a769bbeb4892.herokuapp.com/',
   },
   {
     id: 3,
     name: 'Check Weather Before Travelling',
-    imageUrl: '/src/assets/checktheWeather.png', // Replace with your image path
+    imageUrl: checktheWeather, // Replace with your image path
     githubUrl: 'https://github.com/BaharMahmud/CheckTheWeather  ',
     deployedUrl: 'https://baharmahmud.github.io/CheckTheWeather/',
   },
   {
     id: 4,
     name: 'Webpage for Job Seekers',
-    imageUrl: '/src/assets/findYourDreamJob.png', // Replace with your image path
+    imageUrl: findYourDreamJob, // Replace with your image path
     githubUrl: 'https://github.com/BaharMahmud/FindYourDreamJob',
     deployedUrl: 'https://baharmahmud.github.io/FindYourDreamJob/',
   },
   {
     id: 5,
     name: 'Text Editor Project',
-    imageUrl: 'src/assets/textEditor.png', // Replace with your image path
+    imageUrl: textEditor, // Replace with your image path
     githubUrl: 'https://github.com/BaharMahmud/TectEditorPWAStyle',
     deployedUrl: 'https://texteditorpwaproject19-14daca96dc53.herokuapp.com/',
   },
   {
     id: 6,
     name: 'Note Taker Project',
-    imageUrl: 'src/assets/noteTaker.png', // Replace with your image path
+    imageUrl: noteTaker, // Replace with your image path
     githubUrl: 'https://github.com/BaharMahmud/Note_Taker',
     deployedUrl: 'https://note-taker-app-project-3d19e8dde495.herokuapp.com/',
   },
